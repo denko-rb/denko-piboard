df4eeff17a9a5d960fcfb3587b62f0b6f9e6c916
vickash <vickashmahabir@gmail.com>
2023-05-18 14:31:10 -0400

    Update Board#spi_transfer to match main gem

--- lib/dino/piboard_spi.rb
+++ lib/dino/piboard_spi.rb
@@ -1,58 +1,72 @@
 module Dino
   class PiBoard
 
-    def spi_config(options={})
-      # Config is  a 32-bit mask, where bits 0 and 1 are a 2-bit number equal to the SPI mode.
-      config = options[:mode] || 0b00
+    def spi_config(mode, bit_order)
+      # Config is a 32-bit mask, where bits 0 and 1 are a 2-bit number equal to the SPI mode.
+      # Default to SPI mode 0 when none given.
+      config = mode || 0
       raise ArgumentError, "invalid SPI mode #{config}" unless (0..3).include? config
+
+      # Default to :msbfirst when bit_order not given.
+      bit_order ||= :msbfirst
+      unless (bit_order == :msbfirst) || (bit_order == :lsbfirst)
+        raise ArgumentError, "invalid bit order #{bitorder}"
+      end
+
+      # Bits 14 and 15 control MSBFIRST (0) or LSBFIRST (1) for MOSI and MISO respectively.
+      # Use same order for both directions like Arduino does.
+      config |= (0b11 << 14) if bit_order == :lsbfirst
 
       # Use SPI1 interface instead of SPI0.
       # Setting bit 8 means we're using SPI1.
       config |= (0b1 << 8)
 
       # Bits 5-7 set leaves all CE pins free, and allows any GPIO to be used for chip enable.
-      # We'll toggle separately in #spi_transfer.
+      # We toggle separately in #spi_transfer.
       config |= (0b111 << 5)
-
-      # Bits 14 and 15 control MSBFIRST (0) or LSBFIRST (1) for MOSI and MISO respectively.
-      config |= (0b11 << 14) if options[:bit_order] == :lsbfirst
 
       return config
     end
 
     # CMD = 26
-    def spi_transfer(pin, options={})
-      # Configure and open SPI handle.
-      config = spi_config(options)
-      baud = options[:frequency] || 1000000
-      spi_open(baud, config)
+    def spi_transfer(select_pin, write: [], read: 0, frequency: nil, mode: nil, bit_order: nil)
+      # Default to 1MHz SPI frequency.
+      frequency ||= 1000000
+
+      # Make SPI config mask.
+      config = spi_config(mode, bit_order)
+
+      # Open SPI handle.
+      spi_open(frequency, config)
       
-      # Chip enable low.
-      digital_write(pin, 0)
+      # Chip enable low. select_pin == 255 means no chip enable (mostly for APA102 LEDs).
+      digital_write(select_pin, 0) unless select_pin == 255
 
       # Do the SPI transfer.
-      write_bytes = options[:write].pack("C*")
+      write_bytes = write.pack("C*")
       read_bytes = Pigpio::IF.spi_xfer(pi_handle, spi_handle, write_bytes)
 
       # Close SPI handle.
       spi_close
 
-      # Chip enable high.
-      digital_write(pin, 1)
+      # Chip enable high. select_pin == 255 means no chip enable (mostly for APA102 LEDs).
+      digital_write(select_pin, 1) unless select_pin == 255
 
       # Handle spi_xfer errors.
       raise StandardError, "spi_xfer error, code #{read_bytes}" if read_bytes.class == Integer
 
-      # Deal with any read bytes.
-      if options[:read]
-        # Truncate to the number of bytes requested.
-        read_bytes = read_bytes[0..(options[:read]-1)]
+      # Handle read bytes.
+      if read > 0
+        message = ""
 
-        # Format like dino expects from a microcontroller.
-        message = read_bytes.split("").map { |byte| byte.ord.to_s }.join(",")
+        # Format like a microcontrolelr would. Limit to number of bytes requested.
+        i = 0
+        while i < read
+          message = "#{message},#{read_bytes[i].ord}"
+        end
 
-        # Call update with the message as if coming from the chip select pin.
-        self.update(pin, message)
+        # Call update with the message as if coming from select_pin.
+        self.update(select_pin, message)
       end
     end
 
@@ -68,9 +82,9 @@ module Dino
 
     attr_reader :spi_handle
     
-    def spi_open(baud, config)
+    def spi_open(frequency, config)
       # Give SPI channel as 0 (SPI CE0), even though we are toggling chip enable separately.
-      @spi_handle = Pigpio::IF.spi_open(pi_handle, 0, baud, config)
+      @spi_handle = Pigpio::IF.spi_open(pi_handle, 0, frequency, config)
       raise StandardError, "SPI error, code #{@spi_handle}" if @spi_handle < 0
     end
 
