a40d12e860e595e89c47bdb342c784a6b6113182
vickash <vickashmahabir@gmail.com>
2024-09-19 13:19:34 -0400

    Switch from C to Ruby implementation for 1-Wire

--- lib/denko/piboard.rb
+++ lib/denko/piboard.rb
@@ -3,8 +3,8 @@ require_relative 'piboard_map'
 require_relative 'piboard_base'
 
 require_relative 'piboard_core'
+
 require_relative 'piboard_pulse'
-require_relative 'piboard_one_wire'
 require_relative 'piboard_tone'
 
 require_relative 'piboard_hardware_pwm'
@@ -16,3 +16,4 @@ require_relative 'piboard_spi'
 require_relative 'piboard_led_array'
 
 require_relative 'piboard_i2c_bb'
+require_relative 'piboard_one_wire'
--- lib/denko/piboard_one_wire.rb
+++ lib/denko/piboard_one_wire.rb
@@ -1,23 +1,41 @@
 module Denko
   class PiBoard
-    def one_wire_reset(pin, value=0)
-      result = LGPIO.one_wire_reset(@gpio_handle, pin)
-      self.update(pin, [result]) if value > 0
+    attr_reader :one_wires
+
+    def one_wire_claim(gpio)
+      one_wire = LGPIO::OneWire.new(@gpio_handle, gpio)
+      @one_wires ? @one_wires << one_wire : @one_wires = [one_wire]
+      one_wire
     end
 
-    def one_wire_search(pin, branch_mask)
-      result_array = LGPIO.one_wire_search(@gpio_handle, pin, branch_mask)
-      self.update(pin, result_array)
+    def one_wire_lookup(gpio)
+      if one_wires
+        one_wires.each { |ow| return ow if (gpio == ow.gpio) }
+      end
+      one_wire_claim(gpio)
     end
 
-    def one_wire_write(pin, parasite_power, *data)
-      parasite_int = parasite_power ? 1 : 0
-      LGPIO.one_wire_write(@gpio_handle, pin, parasite_int, data.flatten)
+    def one_wire_reset(gpio, check_presence=0)
+      interface = one_wire_lookup(gpio)
+      presence  = interface.reset ? 0 : 1
+      self.update(gpio, [presence]) if check_presence != 0
     end
 
-    def one_wire_read(pin, num_bytes)
-      result_array = LGPIO.one_wire_read(@gpio_handle, pin, num_bytes)
-      self.update(pin, result_array)
+    def one_wire_search(gpio, branch_mask)
+      interface    = one_wire_lookup(gpio)
+      result_array = interface.search_pass(branch_mask)
+      self.update(gpio, result_array)
+    end
+
+    def one_wire_write(gpio, parasite, *data)
+      interface = one_wire_lookup(gpio)
+      interface.write(data.flatten, parasite: parasite)
+    end
+
+    def one_wire_read(gpio, length)
+      interface    = one_wire_lookup(gpio)
+      result_array = interface.read(length)
+      self.update(gpio, result_array)
     end
   end
 end
