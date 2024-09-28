1e78f96747d54b9c979f5788a3de86f85a69dc0a
vickash <vickashmahabir@gmail.com>
2024-09-28 10:22:29 -0400

    Mutex for each I2C hw bus. Make handles local vars

--- lib/denko/piboard_i2c.rb
+++ lib/denko/piboard_i2c.rb
@@ -5,72 +5,59 @@ module Denko
       65535
     end
 
-    def i2c_mutex
-      @i2c_mutex ||= Mutex.new
-    end
-
-    def i2c_c_error(name, error, index, address)
-      raise StandardError, "lgpio C I2C #{name} error: #{error} for /dev/i2c-#{index} with address 0x#{address.to_s(16)}"
-    end
-
-    def update_i2c(i2c_index, data)
-      dev = hw_i2c_devs[i2c_index]
-      dev.update(data) if dev
-    end
-
     # CMD = 33
-    def i2c_search(i2c_index)
-      i2c_mutex.synchronize do
+    def i2c_search(index)
+      i2c_mutex(index).synchronize do
         found_string = ""
 
         # Address ranges 0..7 and 120..127 are reserved.
         # Try each address in 8..119 (0x08 to 0x77).
         (0x08..0x77).each do |address|
-          i2c_open(i2c_index, address)
-          bytes = LGPIO.i2c_read_device(i2c_handle, 1)
+          handle = i2c_open(index, address)
+          bytes = LGPIO.i2c_read_device(handle, 1)
           found_string << "#{address}:" if bytes[0] > 0
-          i2c_close
+          i2c_close(handle)
         end
         # Remove trailing colon.
         found_string.chop! unless found_string.empty?
 
-        update_i2c(i2c_index, found_string)
+        update_i2c(index, found_string)
       end
     end
 
     # CMD = 34
-    def i2c_write(i2c_index, address, bytes, frequency=nil, repeated_start=false)
-      i2c_mutex.synchronize do
-        raise ArgumentError, "can't write more than #{i2c_limit} bytes to I2C" if bytes.length > i2c_limit
+    def i2c_write(index, address, bytes, frequency=nil, repeated_start=false)
+      i2c_mutex(index).synchronize do
+        raise ArgumentError, "exceeded #{i2c_limit} bytes for #i2c_write" if bytes.length > i2c_limit
 
-        i2c_open(i2c_index, address)
-        result = LGPIO.i2c_write_device(i2c_handle, bytes)
-        i2c_c_error("write", result, i2c_index, address) if result < 0
+        handle = i2c_open(index, address)
+        result = LGPIO.i2c_write_device(handle, bytes)
+        i2c_c_error("write", result, index, address) if result < 0
 
-        i2c_close
+        i2c_close(handle)
       end
     end
 
     # CMD = 35
-    def i2c_read(i2c_index, address, register, read_length, frequency=nil, repeated_start=false)
-      i2c_mutex.synchronize do
+    def i2c_read(index, address, register, read_length, frequency=nil, repeated_start=false)
+      i2c_mutex(index).synchronize do
         raise ArgumentError, "can't read more than #{i2c_limit} bytes to I2C" if read_length > i2c_limit
 
-        i2c_open(i2c_index, address)
+        handle = i2c_open(index, address)
         if register
-          result = LGPIO.i2c_write_device(i2c_handle, register)
-          i2c_c_error("register write (in read)", result, i2c_index, address) if result < 0
+          result = LGPIO.i2c_write_device(handle, register)
+          i2c_c_error("register write (in read)", result, index, address) if result < 0
         end
 
-        bytes = LGPIO.i2c_read_device(i2c_handle, read_length)
-        i2c_close
+        bytes = LGPIO.i2c_read_device(handle, read_length)
+        i2c_close(handle)
 
         # Some error. -42 means read failed, so just return nil.
         if bytes.class == Integer
           if bytes == -42
             message = nil
           else
-            i2c_c_error("read", bytes, i2c_index, address)
+            i2c_c_error("read", bytes, index, address)
           end
         else
           # Format bytes like denko expects from a microcontroller.
@@ -78,22 +65,37 @@ module Denko
         end
 
         # Update the bus as if message came from microcontroller.
-        update_i2c(i2c_index, message)
+        update_i2c(index, message)
       end
     end
 
     private
 
+    def i2c_mutex(index)
+      i2c_mutexes[index] ||= Mutex.new
+    end
+
+    def i2c_mutexes
+      @i2c_mutexes ||= []
+    end
+
     def i2c_open(index, address, flags=0x00)
-      @i2c_handle = LGPIO.i2c_open(index, address, flags)
-      i2c_c_error("open", @i2c_handle, index, address) if @i2c_handle < 0
+      handle = LGPIO.i2c_open(index, address, flags)
+      i2c_c_error("open", handle, index, address) if handle < 0
+      handle
     end
 
-    def i2c_close
-      LGPIO.i2c_close(@i2c_handle)
-      @i2c_handle = nil
+    def i2c_close(handle)
+      LGPIO.i2c_close(handle)
     end
 
-    attr_reader :i2c_handle
+    def i2c_c_error(name, error, index, address)
+      raise StandardError, "lgpio C I2C #{name} error: #{error} for /dev/i2c-#{index} with address 0x#{address.to_s(16)}"
+    end
+
+    def update_i2c(index, data)
+      dev = hw_i2c_devs[index]
+      dev.update(data) if dev
+    end
   end
 end
