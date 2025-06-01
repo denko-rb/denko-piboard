b8f1bb02145880de50a2eec61ff7c91467dad0a8
vickash <vickashmahabir@gmail.com>
2025-06-01 10:16:17 -0400

    Update OLED example

--- examples/display/ssd1306.rb
+++ examples/display/ssd1306.rb
@@ -27,16 +27,26 @@ oled = Denko::Display::SSD1306.new(bus: bus, rotate: true) # address: 0x3C is de
 # select: and dc: pins must be given. reset is optional (can be pulled high instead).
 # oled = Denko::Display::SSD1306.new(bus: bus, pins: {reset: 259, dc: 260, select: chip_select}, rotate: true)
 
+# Transformation features in hardware.
+# oled.reflect_x
+# oled.reflect_y
+oled.rotate
+
 # Draw some text on the OLED's canvas (a Ruby memory buffer).
 canvas = oled.canvas
-canvas.text_cursor = [27,60]
+baseline = 42
+canvas.text_cursor = 27, baseline+15
 canvas.text "Hello World!"
 
 # Add some shapes to the canvas.
-baseline = 40
-canvas.rectangle(10, baseline, 30, -30)
-canvas.circle(66, baseline - 15, 15)
-canvas.triangle(87, baseline, 117, baseline, 102, baseline - 30)
+canvas.rectangle  x: 10, y: baseline,    w: 30, h: -30
+canvas.circle     x: 66, y: baseline-15, r: 15
+canvas.triangle   x1: 87,   y1: baseline,
+                  x2: 117,  y2: baseline,
+                  x3: 102,  y3: baseline-30
+
+# 1px border to test screen edges.
+canvas.rectangle x1: 0, y1: 0, x2: canvas.x_max, y2: canvas.y_max
 
 # Send the canvas to the OLED's graphics RAM so it shows.
 oled.draw
